a53e157cf81b3b3707e71791bbe39266a97efaf8
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2024-12-28 04:42:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os 
 import time
 
-
+delim = "::"
 
 try:
     while True:
         u_input = input()
         
         if not u_input:
-            print("no input")
+            #print("no input")
             continue
+
+        a = u_input.split(delim)
+
+        for i, b in enumerate(a):
+            print(f" {i +1}: {b}")
 
 
 except KeyboardInterrupt:
