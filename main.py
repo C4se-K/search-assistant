b5e1e5e8deb2eafc4536b7439bf4f4a1e6520cce
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2024-12-28 04:38:52 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import os 
+import time
+
+
+
+try:
+    while True:
+        u_input = input()
+        
+        if not u_input:
+            print("no input")
+            continue
+
+
+except KeyboardInterrupt:
+    print("\n system end...")
+finally:
+    ...
+
+
+
+
+
