d1954d9483ba536ba476b33aaa74fc48f639292d
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2024-12-29 12:42:41 -0800

    241229

--- proof_of_concepts/poc_stream_audio.py
+++ proof_of_concepts/poc_stream_audio.py
@@ -3,15 +3,17 @@ from discord.ext import commands
 import wave
 import os
 from dotenv import load_dotenv
-#from .opus import Encoder
 
 
-from threading import Event
+
+import threading
 import time
 
 #overrides
 from discord.voice_client import VoiceClient
+from discord.sinks import RecordingException, Sink
 from discord.opus import DecodeManager, OpusError
+from discord import opus
 
 
 intents = discord.Intents.default()
@@ -20,33 +22,71 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 connections = {}
 
-
+#Overriding run from DecodeManager
 #inspired from the following, under the MIT license
 #https://github.com/Pycord-Development/pycord/blob/master/discord/opus.py#L521
 class ExtendDecodeManager(DecodeManager):
     def run(self):
+        print("start override decoder")
+        print(self._end_thread.is_set())
         while not self._end_thread.is_set():
+            #print(f'looping {len(self.decode_queue)}')
             try:
                 data = self.decode_queue.pop(0)
             except IndexError:
+                #print("error 1")
                 time.sleep(0.001)
                 continue
-
+            print(f"looping {data}")
             try:
                 if data.decrypted_data is None:
                     continue
                 else:
+                    print("got here")
                     data.decoded_data = self.get_decoder(data.ssrc).decode(data.decrypted_data)
             except OpusError:
+                print("error occured while decoding opus")
                 continue
 
+            if data.decoded_data:
+                print(f"{data.decoded_data.hex()}")
 
+#Overriding start_recording from VoiceClient
 #inspired from the following, under the MIT license
 #https://github.com/Pycord-Development/pycord/blob/master/discord/voice_client.py#L52
 class ExtendVoiceClient(VoiceClient):
-    def start_recording(self):
-        print("override complete")
+    def start_recording(self, sink, callback, *args):
+        #print("override complete")
+        if not self.is_connected():
+            raise RecordingException("Not connected to voice channel.")
+        if not isinstance(sink, Sink):
+            raise RecordingException("Must provide a Sink object.")
+        #if not discord.opus.is_loaded():
+           #discord.opus.load_opus('libopus.so')
 
+        self.empty_socket()
+
+        self.decoder = ExtendDecodeManager(self)
+        self.decoder.start()
+
+        self.recording = True
+        self.decode_queue = []
+
+        self.sink = sink
+        sink.init(self)
+
+        t = threading.Thread(
+            target=self.recv_audio,
+            args=(
+                sink,
+                callback,
+                *args,
+            ),
+        )
+        t.start()
+
+async def once_done(_):
+    pass
 
 @bot.command()
 async def join(ctx):
@@ -66,7 +106,7 @@ async def join(ctx):
 
     # start stream
 
-    vc.start_recording()
+    vc.start_recording(discord.sinks.WaveSink(), once_done)
 
 
 
@@ -92,3 +132,4 @@ if __name__ == "__main__":
     load_dotenv()
     ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
     bot.run(ACCESS_TOKEN)
+    
